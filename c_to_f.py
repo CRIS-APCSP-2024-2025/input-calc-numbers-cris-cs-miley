e67ad6b3f787b1e6eaacddbb5732c9e33453b767
Miley <miley@cris.ac.th>
2025-01-14 18:25:04 +0700

    implemented celsius to fahrenheit program

--- c_to_f.py
+++ c_to_f.py
@@ -5,3 +5,12 @@ Assignment: Celcius to Fahrenheit
 This program will prompt the user to input Celcius temperature 
 and output the equivalent Fahrenheit temperature.
 '''
+
+# prompt user to input numeric string and convert to float 
+celsius = float(input("Enter temperature in celsius: "))
+
+# convert the formula from celsius to fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+
+# print the fahrenheit results
+print(str(celsius )+ "degree celcius is equal to "+ str(fahrenheit )+ " degree fahrenheit.")
